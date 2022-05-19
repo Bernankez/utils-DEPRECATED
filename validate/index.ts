a8f5f70bddb0bbe7e42df66096805e3d707401ec
cole <bernankez@qq.com>
2022-05-19 09:05:08 +0800

    update

--- validate/index.ts
+++ validate/index.ts
@@ -10,3 +10,11 @@ export const isPromise = <T = any>(val: unknown): val is Promise<T> => isObject(
 export const isDate = (val: unknown): val is Date => Object.prototype.toString.call(val) === "[object Date]" && !Number.isNaN((val as Date).getTime());
 
 export const isNumeric = (val: string | number): val is string => typeof val === "number" || /^\d+(\.\d+)?$/.test(val);
+
+export const type = (val: any) => {
+  return Object.prototype.toString
+    .call(val)
+    .split(" ")[1]
+    .slice(0, -1)
+    .replace(/^[a-z]{1}/, p => p.toUpperCase());
+};
